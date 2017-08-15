5def15d1e396bd04de3fed2f2596644270d9a29c
Cameron Wheeler <cameron@scelos.com>
2017-08-14 22:06:33 -0700

    Initial commit

---
+++ gather.py
@@ -0,0 +1,25 @@
+import argparse
+import json
+from os import path
+
+from pattern import Pattern
+
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser(description='Aggregate internet facing data from multiple sources and generate queriable records')
+
+    argParser.add_argument('-p', '--pattern', help='path to the JSON encoded gather pattern', required=True)
+
+    args = argParser.parse_args()
+
+    if not path.isfile(args.pattern):
+        print 'Pattern argument must be a JSON encoded pattern file'
+        exit()
+
+    with open(args.pattern, 'r') as f:
+        try:
+            jsonPattern = json.load(f)
+        except:
+            print 'Failed to parse pattern, pattern must be JSON encoded'
+            exit()
+
+        userPattern = Pattern(jsonPattern)
---
+++ pattern.py
@@ -0,0 +1,5 @@
+class Pattern(object):
+    '''A mapping of sources to fields'''
+
+    def __init__(self, json):
+        self.json = json
